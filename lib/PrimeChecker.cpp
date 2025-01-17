b9330198564c1685e855360a8f67311cf8865f9a
Hengameh <hengamehlatifii@gmail.com>
2025-01-17 23:50:25 +0100

    refactor(lib): implement isPrime function

--- lib/PrimeChecker.cpp
+++ lib/PrimeChecker.cpp
@@ -1,6 +1,16 @@
 #include "PrimeChecker.hpp"
+#include <cmath>
 
 bool PrimeChecker::isPrime(int number) const {
-    return false;
+    if (number <= 1) return false;
+    if (number == 2) return true;
+    if (number % 2 == 0) return false;
+
+    int limit = static_cast<int>(std::sqrt(number));
+    for (int i = 3; i <= limit; i += 2) {
+        if (number % i == 0)
+            return false;
+    }
+    return true;
 }
 
